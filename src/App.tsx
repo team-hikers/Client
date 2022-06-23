ab5fb70b0c23dbcedfa67306dc224c1eb82f2621
Dzeko <vivala0519@naver.com>
2022-06-23 18:51:45 +0900

    [feat] Routing 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
+import { useEffect } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import Todo from './pages/Todo';
 import SignUp from './pages/SignIn';
 
 function App() {
+  const navigate = useNavigate();
+  const isToken = true;
+
+  useEffect(() => {
+		if (isToken) {
+			navigate('/');
+		} else {
+			navigate('/signin');
+		}
+	}, [isToken]);
+
   return (
-    <>
-      <Todo />
-      <SignIn />
-      <SignUp />
-    </>
+    <div>
+			<Routes>
+        <Route path='/' element={<Todo />} />
+        <Route path='/signin' element={<SignIn />} />
+        {/* <SignUp /> */}
+      </Routes>
+    </div>
   );
 }
 
