8cfb7ebc3f40c4baa9865eb22b8ef91e0f93ede3
Seonghyeon Moon <shine7329@gmail.com>
2022-06-23 17:56:08 +0900

    [feat] GlobalStyle 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import SignIn from './pages/SignIn';
 import Todo from './pages/Todo';
 import SignUp from './pages/SignIn';
+import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Todo />
       <SignIn />
       <SignUp />
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;  
+    box-sizing: border-box;
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+  li {
+    list-style: none;
+  }
+  button {
+    background: none;
+    border: none;
+    cursor: pointer;
+  }
+  input {
+    border: none;
+    outline: none;
+  }
+  label {
+    display: block;
+  }
+`;
+
+export default GlobalStyle;
