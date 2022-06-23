fbcf9722e3aacd3afd6917e0c84d5e73fd896800
Seonghyeon Moon <shine7329@gmail.com>
2022-06-23 18:08:21 +0900

    [feat] Button 스타일 추가 #4

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,20 @@
+import styled from "styled-components";
+
 type Props = {
   label: string;
   onClick: () => void;
 };
 
 const Button = ({ label, onClick }: Props) => {
-  return <button onClick={onClick}>{label}</button>;
+  return <StyledButton onClick={onClick}>{label}</StyledButton>;
 };
 
 export default Button;
+
+const StyledButton = styled.button`
+  background-color: #6A6A6A;
+  color: #fff;
+  border-radius: 7px;
+  width: 350px;
+  height: 40px;
+`
