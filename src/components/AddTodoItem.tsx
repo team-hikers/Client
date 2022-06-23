b98719f32f7638791b7b4f055b031ea244635e1c
Seonghyeon Moon <shine7329@gmail.com>
2022-06-23 19:21:07 +0900

    [feat] AddTodoItem 스타일 추가 #6

--- src/components/AddTodoItem.tsx
+++ src/components/AddTodoItem.tsx
@@ -1,17 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import styled from 'styled-components';
 
 const AddTodoItem = () => {
   const [inputText, setInputText] = useState('');
 
-  useEffect(() => {
-    console.log(inputText);
-  }, [inputText]);
   return (
-    <>
-      <input type='text' name='contents' onChange={handleChange} />
-      <button onClick={handleClick}>추가</button>
-    </>
+    <Container>
+      <Input type='text' name='contents' onChange={handleChange} />
+      <Button onClick={handleClick}>추가</Button>
+    </Container>
   );
+
   function handleChange({ target: { value } }: { target: { value: string } }) {
     setInputText(value);
   }
@@ -22,3 +21,24 @@ const AddTodoItem = () => {
 };
 
 export default AddTodoItem;
+
+const Container = styled.div`
+  margin-bottom: 10px;
+`
+
+const Input = styled.input`
+  width: 270px;
+  height: 40px;
+  border: 1px solid #6A6A6A;
+  border-top-left-radius: 7px;
+  border-bottom-left-radius: 7px;
+`
+
+const Button = styled.button`
+  width: 80px;
+  height: 40px;
+  background-color: #6A6A6A;
+  color: #fff;
+  border-top-right-radius: 7px;
+  border-bottom-right-radius: 7px;
+`
