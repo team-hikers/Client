7f15956622ba8dbe5adc31baf309a2f070bbd8f7
Seonghyeon Moon <shine7329@gmail.com>
2022-06-23 18:06:28 +0900

    [feat] Input 스타일 추가 #4

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,10 +1,19 @@
+import styled from "styled-components";
+
 type Props = {
   name: string;
   onChange: ({ target: { value, name } }: { target: { value: string; name: string } }) => void;
 };
 
 const Input = ({ name, onChange }: Props) => {
-  return <input type='text' name={name} onChange={onChange} />;
+  return <StyledInput type='text' name={name} onChange={onChange} />;
 };
 
 export default Input;
+
+const StyledInput = styled.input`
+  border: 1px solid #6A6A6A;
+  border-radius: 7px;
+  width: 350px;
+  height: 40px;
+`
